a316252862076da946cc22e35eb1ffdd4cce8c7c
Jiahao Liang <jiahal41@uci.edu>
2022-12-01 23:38:45 -0800

    second version

--- app/main.cpp
+++ app/main.cpp
@@ -6,46 +6,5 @@
 
 int main() 
 {
-	const auto &GRAPH = "self.csv";
-	const auto NUM_REGS = 3;
-	
-	const auto &allocation = assignRegisters(GRAPH, NUM_REGS);
-	IGWriter::write(CSVReader::load(GRAPH), "self.dot", allocation);
-//	const auto &GRAPH = "pairs.csv";
-//	const auto NUM_REGS = 2;
-//	
-//	const auto &allocation = assignRegisters(GRAPH, NUM_REGS);
-//	for(const auto &i : allocation)
-//	{
-//		std::cout << i.first << " " << i.second << "\n";
-//	}
-//	IGWriter::write(CSVReader::load(GRAPH), "pairs.dot", allocation);
-	
-	//EXPECT_TRUE(verifyAllocation(GRAPH, NUM_REGS, allocation));
-	
-//	auto GRAPH = "simple.csv";
-//	InterferenceGraph<Variable> ig = CSVReader::load(GRAPH);
-//	
-//	ig.removeEdge("x", "y");
-//	std::cout << ig.numEdges();
-//	
-//	auto add = [](int a, int b) -> int { return a + b; };
-//	
-//	add(1, 2);
-//	
-	
-//	EXPECT_EQ(ig.numEdges(), 2);
-//	ig.removeEdge("x", "z");
-//	EXPECT_EQ(ig.numEdges(), 1);
-//	ig.removeEdge("z", "y");
-//	EXPECT_EQ(ig.numEdges(), 0);
-//	
-//	EXPECT_EQ(ig.numVertices(), 3);
-//	ig.removeVertex("z");
-//	EXPECT_EQ(ig.numVertices(), 2);
-//	ig.removeVertex("y");
-//	EXPECT_EQ(ig.numVertices(), 1);
-//	ig.removeVertex("x");
-//	EXPECT_EQ(ig.numVertices(), 0);
 	return 0; 
 }
